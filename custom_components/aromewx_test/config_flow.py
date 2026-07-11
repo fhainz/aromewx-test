41ce7c25058749a61bc39f060dbbb7311a3e2f1f
Fabian Hainz <fstoeckl@outlook.com>
2026-07-11 22:23:34 +0200

    Test: NumberSelector mit hass.config.latitude als Default

--- custom_components/aromewx_test/config_flow.py
+++ custom_components/aromewx_test/config_flow.py
@@ -1,8 +1,24 @@
+import voluptuous as vol
+
 from homeassistant.config_entries import ConfigFlow
+from homeassistant.helpers import selector
 
 from .const import DOMAIN
 
 
 class AromeWxTestConfigFlow(ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
-        return self.async_show_form(step_id="user")
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(
+                        "latitude", default=self.hass.config.latitude
+                    ): selector.NumberSelector(
+                        selector.NumberSelectorConfig(
+                            step=0.0001, mode=selector.NumberSelectorMode.BOX
+                        )
+                    ),
+                }
+            ),
+        )
